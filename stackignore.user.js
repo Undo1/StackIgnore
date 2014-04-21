8fc8d0df53eb7c620a61221d49ab0ad93c16767d
KeyboardFire <oddllamaprod@gmail.com>
2014-04-20 19:47:00 -0500

    Cleanup (see extended description) ~Doorknob
    Hey Undo, it's Doorknob :D
    
    I fixed the `$`/`jQuery` weirdness, added some `+=`s and `++`s, changed up some line breaks, fixed some inconsistent styles, abstracted some stuff into functions, made jQuery element creation cleaner, and made readability bettar in general.

--- stackignore.user.js
+++ stackignore.user.js
@@ -29,7 +29,7 @@ function with_jquery(f) {
 	document.body.appendChild(script);
 };
 
-with_jquery(function(jQuery) {
+with_jquery(function($) {
 
 	var arr = $.parseJSON(localStorage["ignoredUsers"]);
 	
@@ -37,27 +37,24 @@ with_jquery(function(jQuery) {
 	localStorage["ignoredUsers"] = JSON.stringify(arr);
 
 	var users = '<div id="ignoredUsersDiv">';
-
-	jQuery.each(arr, function(index, item) {
-		users = users + '<a href="/users/' + item + '" class="post-tag user-tag ignored-user-tag-' + item + '" rel="tag">' + item + '</a>';
+	$.each(arr, function(index, item) {
+		users += '<a href="/users/' + item + '" class="post-tag user-tag ignored-user-tag-' + item + '" rel="tag">' + item + '</a>';
 	});
-	users = users + '</div>';
+	users += '</div>';
 
 	$("div#interesting-tags").after('<div class="module" id="ignored-users"><h4 id="h-ignored-users">Ignored users</h4><div id="ignoredUsers"></div>' + users + '<span id="add-ignored"></span><a id="addIgnoredUser" class="">Add/remove an ignored user</a><br></div></div>');
 
-	$('#addIgnoredUser').bind("click",function(){
+	$('#addIgnoredUser').bind("click", function(){
 		$('#addIgnoredUser').remove();
 		$("#ignored-users").append('<span id="add-ignored-user"><table><tbody><tr><td class="vt"><input type="text" placeholder="user id" id="ignored-user" name="ignored-user" autocomplete="off" class="ac_input"></td><td class="vt"><input id="ignored-user-add" type="button" value="Go"></td> </tr></tbody></table></span>');
 
 		$("#ignored-user-add").bind('click',function(){
 			var userToIgnore = $("#ignored-user").val();
-
 			if (userToIgnore.length < 1) return;
 
 			var arr = $.parseJSON(localStorage["ignoredUsers"]);
-			
 			var i = arr.indexOf(userToIgnore);
-			if(i != -1) {
+			if (i != -1) {
 				arr.splice(i, 1);
 				console.log("yay");
 				$(".ignored-user-tag-" + userToIgnore).remove();
@@ -70,47 +67,58 @@ with_jquery(function(jQuery) {
 			$("#ignoredUsersDiv").append('<a href="/users/' + userToIgnore + '" class="post-tag user-tag ignored-user-tag-' + userToIgnore + '" rel="tag">' + userToIgnore + '</a>')
 		});
 	});
+
 	var numAnswersHidden = 0;
-	jQuery.each(arr, function(index, item) {
+	$.each(arr, function(index, item) {
 		console.log("checking for answers from " + item);
 		var answer = $("div.user-details a[href^='/users/" + item + "']").closest("div[id^=answer]");
-		if (answer.length > 0)
-		{
-			numAnswersHidden = numAnswersHidden + 1;
-			answer.css("background-color", "rgb(220,240,255)");
-			answer.css("display", "none");
-			answer.addClass("hiddenByStackIgnore");
+		if (answer.length > 0) {
+			numAnswersHidden++;
+			stackIgnore(answer);
 		}
 	});
-	if (numAnswersHidden > 0) 
-	{
+	if (numAnswersHidden) {
 		$("h2.bottom-notice.bottom-share-links").css("display", "none");
 		$("div.subheader.answers-subheader h2").append(" <span style='color:grey'> (" + numAnswersHidden + " hidden)</span>");
 
-		$("form.post-form").before("<div class='stackignore-answers-hidden-banner' style='font-size:17px; background-color:rgb(220,240,255); margin:5px; padding:10px; border:3px solid rgb(210,230,245)'>" + numAnswersHidden + ((numAnswersHidden > 1) ? " answers" : " answer") + " hidden by StackIgnore. <a class='stackignore-show-hidden-answers'>Click to show.</a></div>")
+		stackIgnoreBanner($("form.post-form"), numAnswersHidden + ((numAnswersHidden != 1) ? " answers" : " answer") + " hidden by StackIgnore. <a class='stackignore-show-hidden-answers'>Click to show.</a>").addClass('stackignore-answers-hidden-banner');
 		$(".stackignore-show-hidden-answers").bind('click', function() {
 			$("div.hiddenByStackIgnore[id^=answer]").css("display", "block");
 		});
 	}
+	
 	var numCommentsHidden = 0;
-	jQuery.each(arr, function(index, item) {
+	$.each(arr, function(index, item) {
 		console.log("checking for comments from " + item);
 		var comment = $("tr.comment a.comment-user[href^='/users/" + item + "']").closest("tr[id^=comment]");
-		if (comment.length > 0)
-		{
-			numCommentsHidden = numCommentsHidden + 1;
-			comment.css("background-color", "rgb(220,240,255)");
-			comment.css("display", "none");
-			comment.addClass("hiddenByStackIgnore");
+		if (comment.length > 0) {
+			numCommentsHidden++;
+			stackIgnore(comment);
 		}
 	});
-	if (numCommentsHidden > 0) 
-	{
+	if (numCommentsHidden) {
 		$("h2.bottom-notice.bottom-share-links").css("display", "none");
 
-		$("form.post-form").before("<div class='stackignore-comments-hidden-banner' style='font-size:17px; background-color:rgb(220,240,255); margin:5px; padding:10px; border:3px solid rgb(210,230,245)'>" + numCommentsHidden + ((numCommentsHidden > 1) ? " comments" : " comment") + " hidden by StackIgnore. <a class='stackignore-show-hidden-comments'>Click to show.</a></div>")
+		stackIgnoreBanner($("form.post-form"), numCommentsHidden + ((numCommentsHidden > 1) ? " comments" : " comment") + " hidden by StackIgnore. <a class='stackignore-show-hidden-comments'>Click to show.</a>").addClass('stackignore-comments-hidden-banner');
 		$(".stackignore-show-hidden-comments").bind('click', function() {
 			$("tr.comment.hiddenByStackIgnore").css("display", "block");
 		});
 	}
+	
+	function stackIgnore(el) {
+		el.css("background-color", "rgb(220,240,255)");
+		el.css("display", "none");
+		el.addClass("hiddenByStackIgnore");
+	}
+	
+	function stackIgnoreBanner(el, html, class) {
+		return $("<div>" + html + "</div>").css({
+			fontSize: '17px;,
+			backgroundColor: 'rgb(220,240,255)',
+			margin: '5px',
+			padding: '10px',
+			border: '3px solid rgb(210,230,245)'
+		}).insertBefore(el);
+	}
+	
 });
